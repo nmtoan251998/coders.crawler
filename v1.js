da7a47f01ce44237c15cbe6ee9ed48a090d44e8d
nmtoan251998 <nmtoan251998@gmail.com>
2019-05-31 09:52:37 +0700

    v1

---
+++ v1.js
@@ -0,0 +1,51 @@
+/*
+TODO: Performance - Use Promise.all to async tasks
+TODO: Cleancode - Functionalize source code
+*/
+
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+const fullURL = 'https://www.instagram.com/tiffanyjuly10/';
+
+(async () => {
+    try {        
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(fullURL);
+
+        await page.screenshot({ path: __dirname + `/test.png` });
+        await page.pdf({ path: __dirname + `/test.pdf`, format: 'A4' });
+        const DOMContent = await page.content();
+
+        fs.writeFile(__dirname + `/test.html`, DOMContent, function(err) {
+            if(err) return console.log(err);
+        }); 
+
+        let imgSrcsets = await page.evaluate(() => {
+            // Convert Array
+            let imgEls = Array.from(document.querySelectorAll(`img.FFVAD`));
+    
+            const result = imgEls.map(el => ({
+                srcset: el.getAttribute('srcset'),
+                alt: el.getAttribute('alt'),
+                src: el.getAttribute('src'),
+                sizes: el.getAttribute('sizes'),
+                class: el.getAttribute('class')
+            }));
+    
+            return result;
+        })
+
+        const JSONImgSrcsets = imgSrcsets.forEach(data => data)
+
+        fs.writeFile('test.json', JSON.stringify(JSONImgSrcsets, null, '\t'), 'utf8', (err) => {
+            if(err) console.log(err)
+        });
+                
+        await page.waitFor(3000)
+        await browser.close();
+    } catch(err) {
+        console.log(err);
+    }    
+})();
